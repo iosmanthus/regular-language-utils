91f80b772c65c59c2ccdf5271a55a170a8abb058
iosmanthus <myosmanthustree@gmail.com>
2019-04-24 08:37:12 +0800

    add module `nfa` and remove unit tests in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,3 @@
 pub mod ast;
+pub mod nfa;
 pub mod re;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_re_new() {
-        use crate::re::Re;
-        println!("{:#?}", Re::new("1|2|3"));
-    }
-}
