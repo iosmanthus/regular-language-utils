e51cbf34c890e54d6c1c7903ae41ec31c2599149
iosmanthus <myosmanthustree@gmail.com>
2019-04-25 17:22:56 +0800

    make Ast::children optional

--- src/ast.rs
+++ src/ast.rs
@@ -3,11 +3,19 @@ use std::rc::Rc;
 #[derive(Clone, Debug, PartialEq)]
 pub struct Ast<T> {
     token: T,
-    children: Vec<Rc<Ast<T>>>,
+    children: Option<Vec<Rc<Ast<T>>>>,
 }
 
 impl<T> Ast<T> {
-    pub fn new(token: T, children: Vec<Rc<Ast<T>>>) -> Self {
+    pub fn new(token: T, children: Option<Vec<Rc<Ast<T>>>>) -> Self {
         Ast { token, children }
+    }
+
+    pub fn token(&self) -> &T {
+        &self.token
+    }
+
+    pub fn children(&self) -> Option<&Vec<Rc<Ast<T>>>> {
+        self.children.as_ref()
     }
 }
